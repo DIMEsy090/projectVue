3155ae262f828f0f7439f7225198ab26ab12a513
DIMEsy090 <dmitriyy.plesovskih@gmail.com>
2024-02-03 18:46:03 +0300

    https://www.youtube.com/watch?v=XzLuMtDelGk&ab_channel=UlbiTV 46 минута сделал сайт на vue 3 с возможностью создавать посты

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form
+        @create = 'createPost'
+    />
+    <post-list
+        :posts="posts"
+    />
+  </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+export default {
+  components: {
+    PostList,PostForm
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: "Javascript", body: "Описание поста"},
+        {id: 2, title: "Javascript 2", body: "Описание поста 2"},
+        {id: 3, title: "Javascript 3", body: "Описание поста 3"},
+        {id: 3, title: "Javascript 4", body: "Описание поста 4"},
+      ]
+    }
+  },
+  methods:{
+    createPost(post) {
+      this.posts.push(post);
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+
+
+form {
+  display: flex;
+  flex-direction: column;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
 }
+
+.btn{
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
 </style>
